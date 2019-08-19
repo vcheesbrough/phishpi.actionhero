21fc4aab747ded2489aeae5c6ea42e21a63fdfe5
vincent <vincent.cheesbrough@gmail.com>
2019-08-19 01:07:30 +0100

    Refactor to combine channel methods and values into a class

---
+++ public/auto-schedule-channel.js
@@ -0,0 +1,113 @@
+import * as AutoScheduleUtils from './auto-schedule-utils.js'
+
+export class AutoScheduleChannel {
+  #name
+  #canvas
+  #highlighted = false
+  #nodes
+  #schedule
+  #needsDraw = true
+  #dimensions
+  static nodeRadius = 5
+  static nodeFillColour = 'white'
+  static highlightBlurLevel = 10
+  static highlightBlueColour = 'gray'
+  static colourLookup = {
+    red: '#ff0000',
+    blue: '#0008ff',
+    white: '#000000'
+  }
+
+  constructor (channelName, containerElement, dimensions) {
+    this.#name = channelName
+    this.#dimensions = dimensions
+    this.#canvas = AutoScheduleUtils.createCanvasAndAddToContainingElement(this.#name + 'Canvas', 30, containerElement)
+  }
+
+  get name () {
+    return this.#name
+  }
+
+  set highlighted (isHighlighted) {
+    if (this.#highlighted !== isHighlighted) {
+      this.#highlighted = isHighlighted
+      this.#needsDraw = true
+    }
+  }
+
+  get nodes () {
+    if (!this.#nodes) {
+      const ctx = this.#canvas.getContext('2d')
+      this.#needsDraw = true
+      this.#nodes = this.#schedule
+        .orderBy(scheduleItem => scheduleItem.timeMs)
+        .select(scheduleItem => ({
+          coordinate: {
+            x: AutoScheduleUtils.translateTimeMsToX(ctx, scheduleItem.timeMs, this.#dimensions),
+            y: AutoScheduleUtils.translateIntensityToY(ctx, scheduleItem.intensity, this.#dimensions)
+          },
+          schedulePoint: scheduleItem
+        }))
+        .toArray()
+    }
+    return this.#nodes
+  }
+
+  set schedule (schedule) {
+    this.#needsDraw = true
+    this.#schedule = schedule
+    this.#nodes = undefined
+  }
+
+  onResize () {
+    this.#needsDraw = true
+    AutoScheduleUtils.resizeCanvasToFitInParent(this.#canvas)
+    this.#nodes = undefined
+  }
+
+  drawIfNeeded () {
+    if (this.#needsDraw && this.#schedule) {
+      const ctx = this.#canvas.getContext('2d')
+
+      this.#canvas.style.zIndex = this.#highlighted ? '35' : '30'
+      ctx.shadowBlur = this.#highlighted ? AutoScheduleChannel.highlightBlurLevel : 0
+      ctx.shadowColor = AutoScheduleChannel.highlightBlueColour
+
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+      ctx.beginPath()
+      ctx.strokeStyle = AutoScheduleChannel.colourLookup[(this.#name)]
+
+      const firstNode = this.nodes[0]
+      const lastNode = this.nodes[this.nodes.length - 1]
+      const midnightCrossingDeltaTimeMs = firstNode.schedulePoint.timeMs - (0 - lastNode.schedulePoint.timeMs)
+      const midnightCrossingDeltaIntensity = firstNode.schedulePoint.intensity - lastNode.schedulePoint.intensity
+      const midnightCrossingSlopRatio = midnightCrossingDeltaIntensity / midnightCrossingDeltaTimeMs
+
+      ctx.moveTo(
+        AutoScheduleUtils.translateTimeMsToX(ctx, 0, this.#dimensions),
+        AutoScheduleUtils.translateIntensityToY(ctx, firstNode.schedulePoint.intensity - ((firstNode.schedulePoint.timeMs) * midnightCrossingSlopRatio), this.#dimensions))
+
+      const coordinates = Enumerable.from(this.nodes)
+        .select(node => node.coordinate)
+      coordinates
+        .concat([({
+          x: AutoScheduleUtils.translateTimeMsToX(ctx, 86400000, this.#dimensions),
+          y: AutoScheduleUtils.translateIntensityToY(ctx, lastNode.schedulePoint.intensity + lastNode.schedulePoint.timeMs * midnightCrossingSlopRatio, this.#dimensions)
+        })])
+        .forEach(coordinate => {
+          ctx.lineTo(coordinate.x, coordinate.y)
+        })
+      ctx.lineWidth = 2
+      ctx.stroke()
+
+      ctx.fillStyle = this.#highlighted ? AutoScheduleChannel.colourLookup[(this.#name)] : AutoScheduleChannel.nodeFillColour
+      coordinates.forEach(coordinate => {
+        ctx.beginPath()
+        ctx.arc(coordinate.x, coordinate.y, AutoScheduleChannel.nodeRadius, 0, Math.PI * 2)
+        ctx.fill()
+        ctx.stroke()
+      })
+      this.#needsDraw = false
+    }
+  }
+}
--- public/auto-schedule-editor.js
+++ public/auto-schedule-editor.js
@@ -1,10 +1,15 @@
 'use strict'
 
+import * as AutoScheduleUtils from './auto-schedule-utils.js'
+import { AutoScheduleChannel } from './auto-schedule-channel.js'
+
 export function registerAutoTimeCanvas (client, containingElement) {
-  const topGutter = 20
-  const bottomGutter = 50
-  const rightGutter = 20
-  const leftGutter = 80
+  const chartAreaDimensions = {
+    topGutter: 20,
+    bottomGutter:  50,
+    rightGutter:  20,
+    leftGutter: 80
+  }
   const mouseAxisTickLength = 20
   const mouseAxisTickWidth = 10
   const mouseAxisTickColour = '#abaaaa'
@@ -13,159 +18,31 @@ export function registerAutoTimeCanvas (client, containingElement) {
   const mouseFudgeFactor = 10
   const axisColour = '#d9d9d9'
 
-  const createCanvasAndAddToContainingElement = (canvasId, zIndex) => {
-    const canvas = document.createElement("canvas");
-    canvas.id = canvasId;
-    canvas.style.position = 'absolute'
-    canvas.style.zIndex = zIndex
-    canvas.style.width = containingElement.clientWidth
-    canvas.style.height = containingElement.clientHeight
-    canvas.style.left = containingElement.clientX
-    canvas.style.top = containingElement.clientY
-    containingElement.appendChild(canvas);
-    return canvas
-  }
-
-  class Channel {
-    #name
-    #canvas
-    #highlighted = false
-    #nodes
-    #schedule
-    #needsDraw = true
-    static nodeRadius = 5
-    static nodeFillColour = 'white'
-    static highlightBlurLevel = 10
-    static highlightBlueColour = 'gray'
-    static colourLookup = {
-      red: '#ff0000',
-      blue: '#0008ff',
-      white: '#000000'
-    }
-
-    constructor(channelName) {
-      this.#name = channelName
-      this.#canvas = createCanvasAndAddToContainingElement(this.#name + 'Canvas', 30)
-    }
-
-    get name() {
-      return this.#name
-    }
-
-    set highlighted(isHighlighted) {
-      if(this.#highlighted !== isHighlighted) {
-        this.#highlighted = isHighlighted
-        this.#needsDraw = true
-      }
-    }
-
-    get nodes() {
-      if(!this.#nodes) {
-        const ctx = this.#canvas.getContext('2d')
-        this.#needsDraw = true
-        this.#nodes = this.#schedule
-          .orderBy(scheduleItem => scheduleItem.timeMs)
-          .select(scheduleItem => ({
-            coordinate: {
-              x:translateTimeMsToX(ctx, scheduleItem.timeMs),
-              y:translateIntensityToY(ctx, scheduleItem.intensity)
-            },
-            schedulePoint: scheduleItem
-          }))
-          .toArray()
-      }
-      return this.#nodes
-    }
-
-    set schedule(schedule) {
-      this.#needsDraw = true
-      this.#schedule = schedule
-      this.#nodes = undefined
-    }
-
-    onResize() {
-      this.#needsDraw = true
-      resizeCanvas(this.#canvas)
-      this.#nodes = undefined
-    }
-
-    drawIfNeeded() {
-      if(this.#needsDraw && this.#schedule) {
-        const ctx = this.#canvas.getContext('2d')
-
-        this.#canvas.style.zIndex = this.#highlighted ? '35' : '30'
-        ctx.shadowBlur = this.#highlighted ? Channel.highlightBlurLevel : 0
-        ctx.shadowColor = Channel.highlightBlueColour
-
-        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
-        ctx.beginPath()
-        ctx.strokeStyle = Channel.colourLookup[(this.#name)]
-
-        const firstNode = this.nodes[0]
-        const lastNode = this.nodes[this.nodes.length-1]
-        const midnightCrossingDeltaTimeMs = firstNode.schedulePoint.timeMs - (0 - lastNode.schedulePoint.timeMs)
-        const midnightCrossingDeltaIntensity = firstNode.schedulePoint.intensity - lastNode.schedulePoint.intensity
-        const midnightCrossingSlopRatio = midnightCrossingDeltaIntensity / midnightCrossingDeltaTimeMs
-
-        ctx.moveTo(
-          translateTimeMsToX(ctx, 0),
-          translateIntensityToY(ctx, firstNode.schedulePoint.intensity - ((firstNode.schedulePoint.timeMs) * midnightCrossingSlopRatio)))
-
-        const coordinates = Enumerable.from(this.nodes)
-          .select(node => node.coordinate)
-        coordinates
-          .concat([({
-            x: translateTimeMsToX(ctx, 86400000),
-            y: translateIntensityToY(ctx, lastNode.schedulePoint.intensity + lastNode.schedulePoint.timeMs * midnightCrossingSlopRatio)
-          })])
-          .forEach(coordinate => {
-            ctx.lineTo(coordinate.x, coordinate.y)
-          })
-        ctx.lineWidth = 2
-        ctx.stroke()
-
-        ctx.fillStyle = this.#highlighted ? Channel.colourLookup[(this.#name)] : Channel.nodeFillColour
-        coordinates.forEach(coordinate => {
-          ctx.beginPath()
-          ctx.arc(coordinate.x, coordinate.y, Channel.nodeRadius, 0, Math.PI * 2)
-          ctx.fill()
-          ctx.stroke()
-        })
-        this.#needsDraw = false
-      }
-    }
-  }
-
-  const resizeCanvas = (canvas) => {
-    canvas.width = containingElement.clientWidth
-    canvas.height = containingElement.clientHeight
-  }
-
   const channels = {}
 
   Enumerable.from(['red', 'blue', 'white'])
     .forEach(channelName => {
-      channels[channelName] = new Channel(channelName)
+      channels[channelName] = new AutoScheduleChannel(channelName, containingElement, chartAreaDimensions)
     })
 
-  const axisCanvas = createCanvasAndAddToContainingElement('axisCanvas',2)
-  const mouseCanvas = createCanvasAndAddToContainingElement('mouseCanvas',1)
+  const axisCanvas = AutoScheduleUtils.createCanvasAndAddToContainingElement('axisCanvas',2, containingElement)
+  const mouseCanvas = AutoScheduleUtils.createCanvasAndAddToContainingElement('mouseCanvas',1, containingElement)
 
   const convertMouseEventCoordinates = (event) => {
     let x = event.offsetX
-    if(x < leftGutter) {
-      x = leftGutter
+    if(x < chartAreaDimensions.leftGutter) {
+      x = chartAreaDimensions.leftGutter
     }
-    if(x > mouseCanvas.clientWidth - rightGutter)
+    if(x > mouseCanvas.clientWidth - chartAreaDimensions.rightGutter)
     {
-      x = mouseCanvas.clientWidth - rightGutter
+      x = mouseCanvas.clientWidth - chartAreaDimensions.rightGutter
     }
     let y = event.offsetY
-    if(y < topGutter) {
-      y = topGutter
+    if(y < chartAreaDimensions.topGutter) {
+      y = chartAreaDimensions.topGutter
     }
-    if(y > mouseCanvas.clientHeight - bottomGutter) {
-      y = mouseCanvas.clientHeight - bottomGutter
+    if(y > mouseCanvas.clientHeight - chartAreaDimensions.bottomGutter) {
+      y = mouseCanvas.clientHeight - chartAreaDimensions.bottomGutter
     }
     return {x,y}
   }
@@ -184,16 +61,16 @@ export function registerAutoTimeCanvas (client, containingElement) {
     ctx.fillStyle = mouseAxisTickColour
 
     ctx.beginPath()
-    ctx.moveTo(mouseCoordinate.x - mouseAxisTickWidth/2, ctx.canvas.clientHeight - bottomGutter + mouseAxisTickLength)
-    ctx.lineTo(mouseCoordinate.x, ctx.canvas.clientHeight - bottomGutter)
-    ctx.lineTo(mouseCoordinate.x + mouseAxisTickWidth/2, ctx.canvas.clientHeight - bottomGutter + mouseAxisTickLength)
+    ctx.moveTo(mouseCoordinate.x - mouseAxisTickWidth/2, ctx.canvas.clientHeight - chartAreaDimensions.bottomGutter + mouseAxisTickLength)
+    ctx.lineTo(mouseCoordinate.x, ctx.canvas.clientHeight - chartAreaDimensions.bottomGutter)
+    ctx.lineTo(mouseCoordinate.x + mouseAxisTickWidth/2, ctx.canvas.clientHeight - chartAreaDimensions.bottomGutter + mouseAxisTickLength)
     ctx.closePath()
     ctx.fill()
 
     ctx.beginPath()
-    ctx.moveTo(leftGutter - mouseAxisTickLength, mouseCoordinate.y - mouseAxisTickWidth/2)
-    ctx.lineTo(leftGutter, mouseCoordinate.y)
-    ctx.lineTo(leftGutter - mouseAxisTickLength, mouseCoordinate.y + mouseAxisTickWidth/2)
+    ctx.moveTo(chartAreaDimensions.leftGutter - mouseAxisTickLength, mouseCoordinate.y - mouseAxisTickWidth/2)
+    ctx.lineTo(chartAreaDimensions.leftGutter, mouseCoordinate.y)
+    ctx.lineTo(chartAreaDimensions.leftGutter - mouseAxisTickLength, mouseCoordinate.y + mouseAxisTickWidth/2)
     ctx.closePath()
     ctx.fill()
 
@@ -202,13 +79,13 @@ export function registerAutoTimeCanvas (client, containingElement) {
 
     ctx.textBaseline = 'top'
     ctx.textAlign = 'center'
-    const date = new Date(timeMs || translateXToTimeMs(ctx,mouseCoordinate.x))
+    const date = new Date(timeMs || AutoScheduleUtils.translateXToTimeMs(ctx,mouseCoordinate.x,chartAreaDimensions))
     // noinspection JSUnresolvedVariable,JSUnresolvedFunction
-    ctx.fillText(moment.utc(date).format('HH:mm'),mouseCoordinate.x, ctx.canvas.clientHeight - bottomGutter + mouseAxisTickLength + 5)
+    ctx.fillText(moment.utc(date).format('HH:mm'),mouseCoordinate.x, ctx.canvas.clientHeight - chartAreaDimensions.bottomGutter + mouseAxisTickLength + 5)
 
     ctx.textBaseline = 'middle'
     ctx.textAlign = 'right'
-    ctx.fillText(''+Math.round((intensity || translateYToIntensity(ctx, mouseCoordinate.y))*100)+'%', leftGutter - mouseAxisTickLength - 5,mouseCoordinate.y)
+    ctx.fillText(''+Math.round((intensity || AutoScheduleUtils.translateYToIntensity(ctx, mouseCoordinate.y, chartAreaDimensions))*100)+'%', chartAreaDimensions.leftGutter - mouseAxisTickLength - 5,mouseCoordinate.y)
   }
 
   function isEquivalent(a, b) {
@@ -246,7 +123,7 @@ export function registerAutoTimeCanvas (client, containingElement) {
           .forEach(pair => {
             pair.value.highlighted = false
           })
-        drawAllChannels()
+        drawChannelsThatNeedDrawing()
       })
       return
     }
@@ -274,49 +151,29 @@ export function registerAutoTimeCanvas (client, containingElement) {
           .forEach(pair => {
             pair.value.highlighted = firstMatchingNode && firstMatchingNode.schedulePoint.channel === pair.key
           })
-        drawAllChannels()
+        drawChannelsThatNeedDrawing()
       })
     }
     lastMouseCoordinate = coordinate
   }
 
-  const drawAllChannels = () => {
+  const drawChannelsThatNeedDrawing = () => {
     Enumerable.from(channels)
       .forEach(pair => {
         pair.value.drawIfNeeded()
       })
   }
 
-  const translateXToTimeMs = (ctx, x) => {
-    const ratio = 86400000.0 / (ctx.canvas.clientWidth - rightGutter - leftGutter)
-    return Math.floor((x-leftGutter) * ratio)
-  }
-
-  const translateTimeMsToX = (ctx, timeMs) => {
-    const ratio = 86400000.0 / (ctx.canvas.clientWidth - rightGutter - leftGutter)
-    return Math.floor(leftGutter + (timeMs / ratio))
-  }
-
-  const translateYToIntensity = (ctx, y) => {
-    const ratio = 1.0 / (ctx.canvas.clientHeight - topGutter - bottomGutter)
-    return 1.0 - ((y-topGutter) * ratio)
-  }
-
-  const translateIntensityToY = (ctx, intensity) => {
-    const ratio = 1.0 / (ctx.canvas.clientHeight - topGutter - bottomGutter)
-    return Math.floor(ctx.canvas.clientHeight - ((intensity / ratio) + bottomGutter))
-  }
-
   const drawAxis = () => {
     const ctx = axisCanvas.getContext('2d')
     ctx.lineWidth = 1
     ctx.strokeStyle = axisColour
-    ctx.strokeRect(leftGutter, topGutter, ctx.canvas.clientWidth - rightGutter - leftGutter, ctx.canvas.clientHeight - bottomGutter - topGutter)
+    ctx.strokeRect(chartAreaDimensions.leftGutter, chartAreaDimensions.topGutter, ctx.canvas.clientWidth - chartAreaDimensions.rightGutter - chartAreaDimensions.leftGutter, ctx.canvas.clientHeight - chartAreaDimensions.bottomGutter - chartAreaDimensions.topGutter)
     Enumerable.range(0, 25, 60 * 60 * 1000)
       .forEach(timeMs => {
         ctx.beginPath()
-        const x = translateTimeMsToX(ctx, timeMs)
-        const y = translateIntensityToY(ctx, 0)
+        const x = AutoScheduleUtils.translateTimeMsToX(ctx, timeMs, chartAreaDimensions)
+        const y = AutoScheduleUtils.translateIntensityToY(ctx, 0, chartAreaDimensions)
         ctx.moveTo(x, y)
         ctx.lineTo(x, y + 10)
         ctx.stroke()
@@ -332,17 +189,17 @@ export function registerAutoTimeCanvas (client, containingElement) {
         .forEach(oneChannelSchedule => {
           channels[oneChannelSchedule.key()].schedule = oneChannelSchedule
         })
-      drawAllChannels()
+      drawChannelsThatNeedDrawing()
     }
   })
 
   const onResize = () => {
-    resizeCanvas(axisCanvas)
-    resizeCanvas(mouseCanvas)
+    AutoScheduleUtils.resizeCanvasToFitInParent(axisCanvas)
+    AutoScheduleUtils.resizeCanvasToFitInParent(mouseCanvas)
     Enumerable.from(channels)
       .forEach(pair => pair.value.onResize())
     drawAxis()
-    drawAllChannels()
+    drawChannelsThatNeedDrawing()
   }
 
   // noinspection ES6ModulesDependencies,JSUnresolvedFunction
@@ -353,3 +210,4 @@ export function registerAutoTimeCanvas (client, containingElement) {
   onResize()
 
 }
+
